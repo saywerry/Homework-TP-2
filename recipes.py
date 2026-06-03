00ca9594e712365f78d9982245470fbed12aa67b
saywerry <litvinovaekaterina532@gmail.com>
2026-06-03 15:08:59 +0300

    Добавлен класс DietaryRecipe

--- recipes.py
+++ recipes.py
@@ -107,4 +107,15 @@ class ShoppingList:
             new_shopping_list._items.append((ingredient, recipe_title))
         return new_shopping_list
     
+class DietaryRecipe(Recipe):
+    def __init__(self, title, diet_type, ingredients=None):
+        super().__init__(title, ingredients)
+        self.diet_type = diet_type
+    def scale(self, ratio):
+        scaled_recipe = super().scale(ratio)
+        return DietaryRecipe(
+            self.title,
+            self.diet_type,
+            scaled_recipe.ingredients
+        )
     
